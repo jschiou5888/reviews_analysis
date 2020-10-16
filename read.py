175c9abc20798af4757b41fcf98f232b29a2782a
jschiou5888 <jschiou5888@yahoo.com.tw>
2020-10-16 23:06:26 +0800

    add word count

--- read.py
+++ read.py
@@ -8,6 +8,9 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('總共有', len(data), '筆資料')
 
+print(data[0])
+
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -29,6 +32,46 @@ for d in data:
 		good.append(d)
 print('一共有', len(good), '筆留言提到good')
 print(good[0])			
+
+
+# 文字計數
+
+wc = {} # word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:	
+			wc[word] = 1 # 新增key進字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:	
+		print(word, '出現過的次數為: ', wc[word])	
+	else:
+		print('這個字沒有出現過喔!')	
+
+print('感謝使用本查詢功能')		
+
+			
+
+			
+
+
+
+
+
+
+
 
 
 
